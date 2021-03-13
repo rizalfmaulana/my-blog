1e5950b3425a0161fad1f56e44900f28910e613c
Rizal Maulana <rizalfmaulana@gmail.com>
2021-03-13 16:42:11 +0700

    make form submit

---
+++ src/BlogDetails.js
@@ -0,0 +1,25 @@
+import { useParams } from "react-router";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const { data: blog, error, loading } = useFetch(
+    "http://localhost:8000/blogs/" + id
+  );
+
+  return (
+    <div className="blog-details">
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
